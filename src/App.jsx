cc156121d839df65f4762536a702abcf072433c2
royal-15 <royalrajat732@gmail.com>
2024-05-11 23:35:38 +0530

    changed the site title

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 
 const App = () => {
   return <>
-    <div style={{ color: 'white', backgroundColor: 'blue', textAlign: 'center', padding: '10px' }}>
+    <div className='App'>
       working
     </div>
   </>
