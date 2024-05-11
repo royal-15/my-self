a3d3cf5b5eba86627b1e856d29b5a663ae08f90e
royal-15 <royalrajat732@gmail.com>
2024-05-11 20:08:21 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+const App = () => {
+  return <>
+    <div style={{ color: 'white', backgroundColor: 'blue', textAlign: 'center', padding: '10px' }}>
+      working
+    </div>
+  </>
+}
+
+export default App
